f62b1e5d222a5a74e68dd345c8583af5a875f67d
BRIJ <exreply7878@gmail.com>
2023-09-13 20:52:34 +0530

    custom username input

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,27 +1,63 @@
 //                    //
 //    First Visit     //
 //                   //
-const mainViewDiv = document.querySelector('.main-view')
-const greetingView = mainViewDiv.children[0]
+const mainViewDiv = document.querySelector(".main-view");
+const greetingView = mainViewDiv.children[0];
+const modal = document.querySelector(".modal");
+var _user = "";
 
-const isFirstVisit = localStorage.getItem('isFirstVisit')
+const isFirstVisit = localStorage.getItem("isFirstVisit");
 
-if(isFirstVisit == null){
-    // set status for new visit
-    localStorage.setItem("isFirstVisit", 0)
-    
-    // Show Things for new user
-    greet("User")
-    const name = prompt("What's your name?")
+if (isFirstVisit == null) {
+  // set status for new visit
+  localStorage.setItem("isFirstVisit", 0);
 
-    // Set username
-    localStorage.setItem("userName", name)
+  // Show Modal for taking username
+  const modal = document.querySelector(".modal");
+  modal.classList.add("show");
 
+  // What's his/her name?
+  document.getElementById("username").addEventListener("keyup", (e) => {
+    const key = e.key;
+    if (key === "Enter") {
+      const username = e.target.value;
+      localStorage.setItem("username", username);
+      _user = username;
+      modal.classList.remove("show");
+      setGreeting(username);
+      mainViewDiv.classList.add("show");
+    }
+  });
 }
 
-if(isFirstVisit == 0){
-    // Show things for reguler user
-    const userName = localStorage.getItem('userName') || "User"
-    greet(userName)
-    
+  //                //
+ //  Reguler user //
+//              //
+
+
+if (isFirstVisit == 0) {
+  // Show things for reguler user
+  const userName = localStorage.getItem("username");
+  setGreeting(userName);
+  mainViewDiv.classList.add("show");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function to welcome user
+function setGreeting(message) {
+  let greetDiv = document.querySelector(".greeting-view");
+  greetDiv.textContent = `Hello ${message}`;
 }
