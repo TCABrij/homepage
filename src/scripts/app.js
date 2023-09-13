e158cf0590ccc248a8ff203742bc0b2bae57c9d7
BRIJ <exreply7878@gmail.com>
2023-09-13 22:43:03 +0530

    work on favourites

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,7 +3,7 @@
 //                   //
 const mainViewDiv = document.querySelector(".main-view");
 const greetingView = mainViewDiv.children[0];
-const modal = document.querySelector(".modal");
+const modal = document.querySelector(".modal.user");
 var _user = "";
 
 const isFirstVisit = localStorage.getItem("isFirstVisit");
@@ -30,31 +30,62 @@ if (isFirstVisit == null) {
   });
 }
 
-  //                //
- //  Reguler user //
+//                //
+//  Reguler user //
 //              //
-
 
 if (isFirstVisit == 0) {
   // Show things for reguler user
   const userName = localStorage.getItem("username");
   setGreeting(userName);
   mainViewDiv.classList.add("show");
+
+  /////////////////////////////////
+  //   favourite links of User   //
+  ////////////////////////////////
+
+  const favourites = localStorage.getItem("favourites") || "nothing";
+
+  if (favourites === "nothing") {
+    // show no links
+    document.querySelector(".no-links").classList.add("show");
+  } else showFavouritesToUser(favourites);
 }
 
+//   Add favourite links
+const addNewBtn = document.querySelector(".add-new-link");
+addNewBtn.addEventListener("click", addFavouriteLink);
 
+////////////////////
+//   Functions    //
+////////////////////
 
+function addFavouriteLink() {
+  const closeBtn = document.querySelector(".close-btn");
+  const linkModal = document.querySelector(".modal.link");
 
+  //show link wala Modal 
+  linkModal.classList.add("show")
 
+  // finally adding something
+  document.querySelector(".add-to-btn").addEventListener("click", () => {
+    
+    // work resumed
+    console.log("added to favourites");
+    linkModal.classList.remove("show")
 
+  });
 
+  // user don't want to add
+  closeBtn.addEventListener("click", () => {
+    linkModal.classList.remove("show");
+  });
+}
 
-
-
-
-
-
-
+function showFavouritesToUser(obj_as_string) {
+  let favourites = JSON.parse(obj_as_string);
+  console.log(favourites);
+}
 
 // function to welcome user
 function setGreeting(message) {
