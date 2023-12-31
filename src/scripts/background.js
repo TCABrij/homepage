91b743d9cee25eafae67da364c958fea4837c861
TCABrij <exreply7878@gmail.com>
2023-12-31 21:03:08 +0530

    added theme mode & added touch device detection

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,6 +1,16 @@
-const body = document.body
-const randomNum = Math.floor(Math.random()*5+1)
+const body = document.body;
+let randomNum, src;
 
-const src = `url('src/img/walls/wall${randomNum}.jpg')`
+// check if dark Mode is enabled
+let theme = localStorage.getItem("theme");
 
-body.style.backgroundImage = src
+if (theme == "dark") {
+  randomNum = Math.floor(Math.random() * 5 + 1); // We have 6 dark wallpapers
+  src = `url('src/img/walls/dark/dark${randomNum}.jpg')`;
+} else {
+  // Normal Light mode
+  randomNum = Math.floor(Math.random() * 7 + 1); // We have 8 light wallpapers
+  src = `url('src/img/walls/light${randomNum}.jpg')`;
+}
+
+body.style.backgroundImage = src;
